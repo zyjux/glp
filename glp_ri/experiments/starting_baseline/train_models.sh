2e212342c0e5d6bd06fe623568db89abc6ee1e11
Lander Ver Hoef <lander.verhoef@gmail.com>
2026-03-25 21:38:43 +0000

    Corrected log file names in training bash script

--- glp_ri/experiments/starting_baseline/train_models.sh
+++ glp_ri/experiments/starting_baseline/train_models.sh
@@ -11,13 +11,13 @@ EXPERIMENT_DIR="/home/lverhoef/glp/glp_ri/experiments/starting_baseline/"
 # Train CNN while creating the appropriate logs
 printf "\nTraining CNN"
 CFG_FILE="$EXPERIMENT_DIR/test_config_file.yml"
-LOG_FILE="$EXPERIMENT_DIR/cnn_apply.log"
-ERR_FILE="$EXPERIMENT_DIR/cnn_apply.err"
+LOG_FILE="$EXPERIMENT_DIR/cnn.log"
+ERR_FILE="$EXPERIMENT_DIR/cnn.err"
 /home/lverhoef/.local/bin/safepython.sh $TRAIN_SCRIPT $CFG_FILE > $LOG_FILE 2> $ERR_FILE
 
 # Then train GLP CNN
 printf "\nTraining GLP CNN"
 CFG_FILE="$EXPERIMENT_DIR/test_glp_config_file.yml"
-LOG_FILE="$EXPERIMENT_DIR/glp_apply.log"
-ERR_FILE="$EXPERIMENT_DIR/glp_apply.err"
+LOG_FILE="$EXPERIMENT_DIR/glp.log"
+ERR_FILE="$EXPERIMENT_DIR/glp.err"
 /home/lverhoef/.local/bin/safepython.sh $TRAIN_SCRIPT $CFG_FILE > $LOG_FILE 2> $ERR_FILE
