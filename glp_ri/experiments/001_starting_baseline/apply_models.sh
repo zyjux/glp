1cc2a3257fbf3391a6d89dc741385a8809031a7b
Lander Ver Hoef <lander.verhoef@gmail.com>
2026-03-26 17:09:54 +0000

    Retrain and renamed baseline experiment folder

---
+++ glp_ri/experiments/001_starting_baseline/apply_models.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+# Activate conda environment
+eval "$(conda shell.bash hook)"
+conda activate glp
+
+APPLY_SCRIPT="/home/lverhoef/glp/glp_ri/apply_model.py"
+
+DATA_DIR="/mnt/data2/lverhoef/RI/learning_examples/rotated_with_storm_motion/imputed/normalized"
+DATA_FILE="valid_labels.json"
+
+EXPERIMENT_DIR="/home/lverhoef/glp/glp_ri/experiments/starting_baseline/"
+
+# Train CNN while creating the appropriate logs
+printf "\nApplying CNN"
+CFG_FILE="$EXPERIMENT_DIR/test_config_file.yml"
+OUT_FILE="$EXPERIMENT_DIR/cnn_validation.nc"
+LOG_FILE="$EXPERIMENT_DIR/cnn_apply.log"
+ERR_FILE="$EXPERIMENT_DIR/cnn_apply.err"
+/home/lverhoef/.local/bin/safepython.sh $APPLY_SCRIPT $CFG_FILE $OUT_FILE \
+  --data_dir=$DATA_DIR --data_file=$DATA_FILE --batch_size=32 > $LOG_FILE 2> $ERR_FILE
+
+# Then train GLP CNN
+printf "\nApplying GLP CNN"
+CFG_FILE="$EXPERIMENT_DIR/test_glp_config_file.yml"
+OUT_FILE="$EXPERIMENT_DIR/glp_validation.nc"
+LOG_FILE="$EXPERIMENT_DIR/glp_apply.log"
+ERR_FILE="$EXPERIMENT_DIR/glp_apply.err"
+/home/lverhoef/.local/bin/safepython.sh $APPLY_SCRIPT $CFG_FILE $OUT_FILE \
+  --data_dir=$DATA_DIR --data_file=$DATA_FILE --batch_size=32 > $LOG_FILE 2> $ERR_FILE
