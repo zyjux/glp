5d0386c0420f79a2000e0ae03aa77af50b294fd8
Lander Ver Hoef <lander.verhoef@gmail.com>
2026-04-02 19:49:54 +0000

    Trained experiment four models

---
+++ glp_ri/experiments/004_up_down_glp_encoder/train_models.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+# Activate conda environment
+eval "$(conda shell.bash hook)"
+conda activate glp
+
+TRAIN_SCRIPT="/home/lverhoef/glp/glp_ri/train_model.py"
+
+EXPERIMENT_DIR="/home/lverhoef/glp/glp_ri/experiments/004_up_down_glp_encoder"
+
+# Train CNN while creating the appropriate logs
+printf "\nTraining CNN"
+CFG_FILE="$EXPERIMENT_DIR/test_config_file.yml"
+LOG_FILE="$EXPERIMENT_DIR/cnn.log"
+ERR_FILE="$EXPERIMENT_DIR/cnn.err"
+/home/lverhoef/.local/bin/safepython.sh $TRAIN_SCRIPT $CFG_FILE > $LOG_FILE 2> $ERR_FILE
+
+# Then train GLP CNN
+printf "\nTraining GLP CNN"
+CFG_FILE="$EXPERIMENT_DIR/test_glp_config_file.yml"
+LOG_FILE="$EXPERIMENT_DIR/glp.log"
+ERR_FILE="$EXPERIMENT_DIR/glp.err"
+/home/lverhoef/.local/bin/safepython.sh $TRAIN_SCRIPT $CFG_FILE > $LOG_FILE 2> $ERR_FILE
