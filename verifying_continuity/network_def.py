3bf74fb1a6662a8dc55aee6e836800fb6d8ab2c7
Lander Ver Hoef <lander.verhoef@gmail.com>
2026-06-08 23:58:32 +0000

    Added code to verify continuity of pooling

---
+++ verifying_continuity/network_def.py
@@ -0,0 +1,103 @@
+import torch
+from torch import nn
+from torchvision.transforms.functional import rotate
+
+
+class crps_loss(nn.Module):
+    def __init__(self, reduction: str = "mean"):
+        super().__init__()
+        self.reduction_f = {
+            "none": nn.Identity(),
+            "mean": torch.mean,
+            "sum": torch.sum,
+        }.get(reduction)
+
+    def forward(self, predicted: torch.Tensor, target: torch.Tensor):
+        return self.reduction_f(
+            torch.mean(torch.abs(predicted - torch.unsqueeze(target, dim=-1)), dim=-1)
+            - 0.5
+            * torch.mean(
+                torch.abs(
+                    torch.unsqueeze(predicted, -1) - torch.unsqueeze(predicted, -2)
+                ),
+                dim=(-1, -2),
+            )
+        )  # type: ignore
+
+
+class glp_rotation_stack(nn.Module):
+    def __init__(self, num_angles=None, angle_inc=None):
+        super().__init__()
+        if angle_inc is not None:
+            self.num_angles = 360 / angle_inc
+            self.angle_inc = angle_inc
+        elif num_angles is not None:
+            self.angle_inc = 360 / num_angles
+            self.num_angles = num_angles
+        else:
+            raise ValueError("Need either num_angles or angle_inc to be defined")
+
+    def forward(self, image):
+        temp_list = [
+            rotate(image, self.angle_inc * i) for i in range(int(self.num_angles))
+        ]
+        return torch.stack(temp_list, dim=-1)
+
+
+class glp_rotation_pool(nn.Module):
+    def __init__(self, glp_pooling_size: int, **kwargs):
+        super().__init__()
+        self.kernel_size = glp_pooling_size
+
+    def forward(self, image):
+        num_angles = image.shape[-1]
+        angle_increment = 360 / num_angles
+        output_image = torch.empty(image.shape, device=image.device)
+        for i in range(num_angles):
+            relative_offset = i % self.kernel_size
+            output_image[..., i] = rotate(
+                image[..., i], -angle_increment * relative_offset
+            )
+        return nn.MaxPool3d(kernel_size=(1, 1, self.kernel_size))(output_image)
+
+
+class glp_linear(nn.Module):
+    def __init__(
+        self, in_features: int, out_features: int, bias=True, device=None, dtype=None
+    ):
+        super().__init__()
+        self.linear = nn.Linear(
+            in_features=in_features,
+            out_features=out_features,
+            bias=bias,
+            device=device,
+            dtype=dtype,
+        )
+
+    def forward(self, in_stack):
+        return torch.stack(
+            [self.linear(torch.squeeze(x)) for x in torch.split(in_stack, 1, dim=-1)],
+            dim=-1,
+        )
+
+
+class simple_model(nn.Module):
+    def __init__(self, G_angle_inc: int, num_lambda_cosets: int, psi_size: int = 3):
+        super().__init__()
+        padding = int(psi_size / 2)
+
+        self.setup_layer = glp_rotation_stack(angle_inc=G_angle_inc)
+        self.convolution = nn.Conv3d(
+            in_channels=1,
+            out_channels=1,
+            kernel_size=(psi_size, psi_size, 1),
+            padding=(padding, padding, 0),
+            padding_mode="reflect",
+        )
+        self.pooling = glp_rotation_pool(num_lambda_cosets)
+
+    def forward(self, x):
+        setup_out = self.setup_layer(x)
+        conv_out = self.convolution(setup_out)
+        pool_out = self.pooling(conv_out)
+        return pool_out
---
+++ verifying_continuity/run_test.py
@@ -0,0 +1,109 @@
+import argparse
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Literal
+
+import matplotlib.pyplot as plt
+import torch
+import xarray as xr
+import yaml
+from network_def import simple_model
+from torchinfo import summary
+from torchvision.transforms.functional import rotate
+
+parser = argparse.ArgumentParser(prog="RunTests", description="Runs tests")
+parser.add_argument("cfg", help="Config file", type=str)
+parser.add_argument("exp_dir", help="Directory of experiment directory", type=str)
+args = parser.parse_args()
+
+
+@dataclass
+class configs:
+    g_angle_inc: int
+    num_lambda_cosets: int
+    psi_size: int
+    thetas: list[float]
+    manual_seed: int
+    input_source: Literal["random", "ellipse"]
+    num_input_samples: int
+
+
+def compare_rotated_outputs(outputs: torch.Tensor):
+    ref_output = outputs[0, ...]
+    rotated_output = outputs[1:, ...]
+    diffs = torch.abs(rotated_output - ref_output)
+    return torch.amax(diffs, dim=tuple(range(1, diffs.ndim)))
+
+
+# Load experiment configuration file
+with open(args.cfg, "r") as f:
+    cfg = configs(**yaml.safe_load(f))
+
+torch.manual_seed(cfg.manual_seed)
+torch.cuda.manual_seed(cfg.manual_seed)
+
+if cfg.input_source == "ellipse":
+    indices = torch.randint(10000, (cfg.num_input_samples,))
+elif cfg.input_source == "random":
+    random_images = torch.randn((cfg.num_input_samples, 1, 1, 128, 128))
+
+# Detect gpu
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available() else "cpu"
+)
+print(f"Using {device} device")
+
+# Set up plot
+F, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.set_xlabel("Theta")
+ax.set_ylabel("Max difference")
+
+# Create model
+model = simple_model(
+    G_angle_inc=cfg.g_angle_inc,
+    num_lambda_cosets=cfg.num_lambda_cosets,
+    psi_size=cfg.psi_size,
+)
+
+summary(model, input_size=(1, 128, 128))
+
+for sample in range(cfg.num_input_samples):
+    # Generate input
+    if cfg.input_source == "random":
+        input_f = random_images[sample, ...].to(device)
+        print(f"\n Processing random sample {sample}")
+    elif cfg.input_source == "ellipse":
+        input_f = torch.tensor(
+            xr.open_dataset("/mnt/data2/lverhoef/synthetic_ellipses/train_valid_ds.nc")
+            .isel(sample=indices[sample])["ellipse"]
+            .to_numpy(),
+            dtype=torch.float,
+            device=device,
+        ).view(1, 1, 128, 128)
+        print(f"\n Processing ellipse {indices[sample]}")
+
+    # Create stacked rotations of input
+    input_gf = torch.cat(
+        [input_f] + [rotate(input_f, theta) for theta in cfg.thetas], dim=0
+    )
+
+    # Apply model
+    with torch.no_grad():
+        outputs = model(input_gf)
+
+    # Compare rotated elements against original
+    max_diffs = compare_rotated_outputs(outputs)
+    # max_diffs /= (input_f**2).sum()
+
+    # Print values
+    print("\nTheta | Difference")
+    print("------------------")
+    for i, theta in enumerate(cfg.thetas):
+        print(f"{theta: 6d} | {max_diffs[i].item():.7f}")
+
+    ax.plot(cfg.thetas, max_diffs.cpu())
+
+# Save completed figure
+F.savefig(Path(args.exp_dir, "difference_curve.png"), bbox_inches="tight", dpi=300)
