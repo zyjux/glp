e5d7de9714542c912a462edd6e866363685a2374
Lander Ver Hoef <lander.verhoef@gmail.com>
2026-06-09 17:32:14 +0000

    Added functionality to run tests with TC data

--- verifying_continuity/run_test.py
+++ verifying_continuity/run_test.py
@@ -1,9 +1,12 @@
 import argparse
+import json
+import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
 
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import xarray as xr
 import yaml
@@ -16,6 +19,8 @@ parser.add_argument("cfg", help="Config file", type=str)
 parser.add_argument("exp_dir", help="Directory of experiment directory", type=str)
 args = parser.parse_args()
 
+TC_DATA_DIR = "/mnt/data2/lverhoef/RI/learning_examples/rotated_with_storm_motion/imputed/normalized/"
+
 
 @dataclass
 class configs:
@@ -23,9 +28,10 @@ class configs:
     num_lambda_cosets: int
     thetas: list[float]
     manual_seed: int
-    input_source: Literal["random", "ellipse"]
+    input_source: Literal["random", "ellipse", "tc"]
     num_input_samples: int
     img_size: tuple[int, int]
+    plot_fn: str = "difference_plot.png"
 
 
 def compare_rotated_outputs(outputs: torch.Tensor):
@@ -33,6 +39,63 @@ def compare_rotated_outputs(outputs: torch.Tensor):
     rotated_output = outputs[1:, ...]
     diffs = torch.abs(rotated_output - ref_output)
     return torch.amax(diffs, dim=tuple(range(1, diffs.ndim)))
+
+
+def find_file(directory_name, cyclone_id_string, raise_error_if_missing=True):
+    """Finds NetCDF file with learning examples.
+
+    :param directory_name: Name of directory with example files. Must be absolute path
+    :param cyclone_id_string: Cyclone ID (must be accepted by
+        `parse_cyclone_id`).
+    :param raise_error_if_missing: Boolean flag.  If file is missing and
+        `raise_error_if_missing == True`, will throw error.  If file is missing
+        and `raise_error_if_missing == False`, will return *expected* file path.
+    :return: example_file_name: File path.
+    :raises: ValueError: if file is missing
+        and `raise_error_if_missing == True`.
+    """
+
+    assert (
+        type(directory_name) is str
+    ), f"directory_name must be str; got {type(directory_name)}"
+    assert (
+        type(raise_error_if_missing) is bool
+    ), f"raise_error_if_missing must be bool; got {type(raise_error_if_missing)}"
+
+    example_file_name = f"{directory_name}/learning_examples_{cyclone_id_string}.nc"
+
+    if os.path.isfile(example_file_name) or not raise_error_if_missing:
+        return example_file_name
+
+    error_string = f'Cannot find file.  Expected at: "{example_file_name}"'
+    raise ValueError(error_string)
+
+
+def load_labels(
+    fn: str | Path,
+):
+    """Data utility to parse labels json file into numpy array
+
+    args:
+        fn (str): Filename of the json dictionary to load.
+
+    returns:
+        labels (np.ndarray): A 3xN array, where N is the number of samples in the json
+            file. The first column contains storm ids, the second column contains
+            timestamps, and the third column contains the label.
+
+    """
+    with open(fn, "r") as f:
+        raw_dict = json.load(f)
+
+    labels = []
+    for storm_id in raw_dict.keys():
+        for timestamp in raw_dict[storm_id].keys():
+            labels.append((storm_id, timestamp, raw_dict[storm_id][timestamp]))
+
+    labels = np.array(labels, dtype=object)
+
+    return labels
 
 
 # Load experiment configuration file
@@ -46,6 +109,11 @@ if cfg.input_source == "ellipse":
     indices = torch.randint(10000, (cfg.num_input_samples,))
 elif cfg.input_source == "random":
     random_images = torch.randn((cfg.num_input_samples, 1, 1, *cfg.img_size))
+elif cfg.input_source == "tc":
+    labels_file = Path(TC_DATA_DIR, "valid_labels.json")
+    labels = load_labels(labels_file)
+    indices = torch.randint(labels.shape[0], (cfg.num_input_samples,))
+    cfg.img_size = (380, 540)
 
 # Detect gpu
 device = (
@@ -83,6 +151,14 @@ for sample in range(cfg.num_input_samples):
             device=device,
         ).view(1, 1, *cfg.img_size)
         print(f"\n Processing ellipse {indices[sample]}")
+    elif cfg.input_source == "tc":
+        storm_id, timestamp, _ = labels[indices[sample]]
+        ds = xr.open_dataset(find_file(TC_DATA_DIR, storm_id)).sel(
+            satellite_valid_time_unix_sec=int(timestamp)
+        )
+        input_f = torch.tensor(
+            ds.satellite_predictors_gridded.values.astype(np.float32), device=device
+        ).view(1, 1, *cfg.img_size)
 
     # Create stacked rotations of input
     input_gf = torch.cat(
@@ -107,4 +183,4 @@ for sample in range(cfg.num_input_samples):
     ax.plot(cfg.thetas, max_diffs.cpu())
 
 # Save completed figure
-F.savefig(Path(args.exp_dir, "difference_curve.png"), bbox_inches="tight", dpi=300)
+F.savefig(Path(args.exp_dir, cfg.plot_fn), bbox_inches="tight", dpi=300)
