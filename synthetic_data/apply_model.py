770606989503011fc186c4536a1e0d46d390fd45
Lander Ver Hoef <lander.ver_hoef@colostate.edu>
2026-07-06 22:36:22 +0000

    Moved main routine inside function called from if __name__ is main statement

--- synthetic_data/apply_model.py
+++ synthetic_data/apply_model.py
@@ -17,73 +17,78 @@ parser.add_argument("--data_file", default=Path(DATA_FILE), type=Path)
 parser.add_argument("--batch_size", type=int)
 args = parser.parse_args()
 
-# Load config file
-with open(args.config_file, "r") as f:
-    cfg = model_config(**yaml.safe_load(f))
 
-loss_function_translation = {"crps": crps_loss}
+def main():
+    # Load config file
+    with open(args.config_file, "r") as f:
+        cfg = model_config(**yaml.safe_load(f))
 
-# Load dataset
-ds = xr.load_dataset(DATA_FILE)
+    loss_function_translation = {"crps": crps_loss}
 
-# Categorize angles
-ds["target"] = (
-    "sample",
-    angle_categorizer(
-        ds.angle.values,
-        cfg.training_hyperparameters["num_classes"],
-        diagonal=cfg.training_hyperparameters["diagonal_categorization"],
-    ),
-)
+    # Load dataset
+    ds = xr.load_dataset(DATA_FILE)
 
-# Get cpu, gpu or mps device for training.
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
-)
-print(f"Using {device} device")
+    # Categorize angles
+    ds["target"] = (
+        "sample",
+        angle_categorizer(
+            ds.angle.values,
+            cfg.training_hyperparameters["num_classes"],
+            diagonal=cfg.training_hyperparameters["diagonal_categorization"],
+        ),
+    )
 
-pt_ds = Ellipse_Dataset(ds, start_idx=8000)
+    # Get cpu, gpu or mps device for training.
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    print(f"Using {device} device")
 
-if args.batch_size is None:
-    batch_size = cfg.training_hyperparameters["batch_size"]
-else:
-    batch_size = args.batch_size
-dataloader = DataLoader(pt_ds, num_workers=8, batch_size=batch_size)
+    pt_ds = Ellipse_Dataset(ds, start_idx=9000)
 
-model = CNN(cfg)
-summary(model, input_size=(batch_size, 1, 128, 128))
+    if args.batch_size is None:
+        batch_size = cfg.training_hyperparameters["batch_size"]
+    else:
+        batch_size = args.batch_size
+    dataloader = DataLoader(pt_ds, num_workers=8, batch_size=batch_size)
 
-model.load_state_dict(torch.load(cfg.model_save_file, weights_only=True))
-model.to(device)
+    model = CNN(cfg)
+    summary(model, input_size=(batch_size, 1, 128, 128))
+
+    model.load_state_dict(torch.load(cfg.model_save_file, weights_only=True))
+    model.to(device)
+
+    print(f"Applying model to {Path(args.data_file)} \n")
+    t_time_start = perf_counter()
+    model.eval()
+    with torch.no_grad():
+        for X, y in dataloader:
+            X = X.to(device)
+            X = X.view(-1, 1, 128, 128)
+            y = y.view(-1)
+            pred = model(X).to("cpu")
+            try:
+                preds = torch.cat((preds, pred), dim=0)
+                truth = torch.cat((truth, y), dim=0)
+            except NameError:
+                preds = pred
+                truth = y
+    t_time = perf_counter() - t_time_start
+    print(
+        f"Done! Total evaluation time: {t_time // 60:.0f} minutes, {t_time % 60:.2f} seconds"
+    )
+
+    print("Saving...")
+    preds_ds = xr.Dataset(
+        data_vars={
+            "predictions": (("example", "class"), preds),
+            "labels": ("example", truth),
+        }
+    )
+    preds_ds.to_netcdf(args.save_file)
 
 
-print(f"Applying model to {Path(args.data_file)} \n")
-t_time_start = perf_counter()
-model.eval()
-with torch.no_grad():
-    for X, y in dataloader:
-        X = X.to(device)
-        X = X.view(-1, 1, 128, 128)
-        y = y.view(-1)
-        pred = model(X).to("cpu")
-        try:
-            preds = torch.cat((preds, pred), dim=0)
-            truth = torch.cat((truth, y), dim=0)
-        except NameError:
-            preds = pred
-            truth = y
-t_time = perf_counter() - t_time_start
-print(
-    f"Done! Total evaluation time: {t_time // 60:.0f} minutes, {t_time % 60:.2f} seconds"
-)
-
-print("Saving...")
-preds_ds = xr.Dataset(
-    data_vars={
-        "predictions": (("example", "class"), preds),
-        "labels": ("example", truth),
-    }
-)
-preds_ds.to_netcdf(args.save_file)
+if __name__ == "__main__":
+    main()
--- synthetic_data/train_model.py
+++ synthetic_data/train_model.py
@@ -17,122 +17,136 @@ parser = argparse.ArgumentParser(prog="TrainModel", description="Trains GLP mode
 parser.add_argument("config_file")
 args = parser.parse_args()
 
-# Load config file
-with open(args.config_file, "r") as f:
-    cfg = model_config(**yaml.load(f, Loader=yaml.SafeLoader))
 
-loss_function_translation = {"crps": crps_loss}
+def main():
+    # Load config file
+    with open(args.config_file, "r") as f:
+        cfg = model_config(**yaml.load(f, Loader=yaml.SafeLoader))
 
-hyperparam_config = cfg.training_hyperparameters
+    loss_function_translation = {"crps": crps_loss}
 
-# Load dataset
-ds = xr.load_dataset(DATA_FILE)
+    hyperparam_config = cfg.training_hyperparameters
 
-total_samples = ds.sizes["sample"]
-try:
-    desired_samples = int(total_samples * hyperparam_config["ds_size_ratio"])
-except KeyError:
-    pass
-else:
-    rng = np.random.default_rng()
-    sample_indices = rng.choice(
-        total_samples, desired_samples, replace=False, shuffle=False
-    )
-    ds = ds.isel(sample=sample_indices)
-    print(f"Using reduced dataset of {desired_samples} examples")
-
-# Categorize angles
-ds["target"] = (
-    "sample",
-    angle_categorizer(
-        ds.angle.values,
-        hyperparam_config["num_classes"],
-        diagonal=hyperparam_config["diagonal_categorization"],
-    ),
-)
-
-# Get cpu, gpu or mps device for training.
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
-)
-print(f"Using {device} device")
-
-aug_config = cfg.data_augmentation
-transform_list = []
-if aug_config is not None:
-    if "rotations" in aug_config.keys():
-        rotate_transform = tvtf.RandomRotation(aug_config["rotations"]["max_degrees"])  # type: ignore
-        transform_list += [rotate_transform] * aug_config["rotations"]["num_rotations"]
-    if "noisings" in aug_config.keys():
-        noise_transform = AddGaussianNoise(
-            std=aug_config["noisings"]["standard_deviation"]
-        )
-        transform_list += [noise_transform] * aug_config["noisings"]["num_noisings"]
-    if "translations" in aug_config.keys():
-        max_shift = aug_config["translations"]["max_shift"]
-        translate_transform = tvtf.RandomAffine(0, translate=(max_shift, max_shift))  # type: ignore
-        transform_list += [translate_transform] * aug_config["translations"][
-            "num_translations"
-        ]
-transform_list = tuple(transform_list)
-
-cnn_train_ds = Ellipse_Dataset(ds, end_idx=8000, transforms=transform_list)
-cnn_valid_ds = Ellipse_Dataset(ds, start_idx=8000)
-
-batch_size = hyperparam_config["batch_size"]
-cnn_train_dataloader = DataLoader(cnn_train_ds, num_workers=8, batch_size=batch_size)
-cnn_valid_dataloader = DataLoader(cnn_valid_ds, num_workers=8, batch_size=batch_size)
-
-cnn_model = CNN(cfg).to(device)
-cnn_loss_fn = loss_function_translation[hyperparam_config["loss_function"]]()
-cnn_optimizer = torch.optim.Adam(
-    cnn_model.parameters(), lr=hyperparam_config["learning_rate"]
-)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(cnn_optimizer, "min")
-
-if hyperparam_config["early_stopping"]["enabled"]:
-    early_stopper = EarlyStopper(
-        patience=hyperparam_config["early_stopping"]["patience"],
-        min_delta=hyperparam_config["early_stopping"]["min_delta"],
+    # Load dataset
+    full_ds = xr.load_dataset(DATA_FILE)
+    # Categorize angles
+    full_ds["target"] = (
+        "sample",
+        angle_categorizer(
+            full_ds.angle.values,
+            hyperparam_config["num_classes"],
+            diagonal=hyperparam_config["diagonal_categorization"],
+        ),
     )
 
-summary(cnn_model, input_size=(batch_size, 1, 128, 128))
+    ds = full_ds.isel(sample=slice(8000))
+    valid_ds = full_ds.isel(sample=slice(9000, None))
 
-epochs = hyperparam_config["epochs"]
-print("Training CNN \n")
-t_time_start = perf_counter()
-for t in range(epochs):
-    print(f"Epoch {t + 1}\n----------------------------")
-    start_time = perf_counter()
-    train(
-        cnn_train_dataloader,
-        cnn_model,
-        cnn_loss_fn,
-        cnn_optimizer,
-        accumulation_batches=hyperparam_config["accumulation_batches"],
-        device=device,
-    )
-    val_loss, val_accuracy = validate(
-        cnn_valid_dataloader, cnn_model, cnn_loss_fn, device=device
-    )
-    scheduler.step(val_loss)
-    if hyperparam_config["early_stopping"]["enabled"]:
-        if early_stopper.early_stop(val_loss, val_accuracy):
-            print(
-                f"Early stopping with minimum validation loss of {early_stopper.min_validation_loss}"
-                f", max accuracy of {early_stopper.max_accuracy}"
-            )
-            break
-        if early_stopper.counter == 0:
-            print(f"Validation loss improved, saving model")
-            torch.save(cnn_model.state_dict(), cfg.model_save_file)
+    total_samples = ds.sizes["sample"]
+    try:
+        desired_samples = int(total_samples * hyperparam_config["ds_size_ratio"])
+    except KeyError:
+        pass
     else:
-        torch.save(cnn_model.state_dict(), cfg.model_save_file)
+        rng = np.random.default_rng(hyperparam_config["random_seed"])
+        sample_indices = rng.choice(
+            total_samples, desired_samples, replace=False, shuffle=False
+        )
+        ds = ds.isel(sample=sample_indices)
+        print(f"Using reduced dataset of {desired_samples} examples")
 
-    print(f"Epoch time: {perf_counter() - start_time:.2f} seconds \n")
-t_time = perf_counter() - t_time_start
-print(
-    f"Done! Total training time: {t_time // 60:.0f} minutes, {t_time % 60:.2f} seconds, average epoch time: {t_time/epochs:.2f} seconds"
-)
+    # Get cpu, gpu or mps device for training.
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+    print(f"Using {device} device")
+
+    aug_config = cfg.data_augmentation
+    transform_list = []
+    if aug_config is not None:
+        if "rotations" in aug_config.keys():
+            rotate_transform = tvtf.RandomRotation(aug_config["rotations"]["max_degrees"])  # type: ignore
+            transform_list += [rotate_transform] * aug_config["rotations"][
+                "num_rotations"
+            ]
+        if "noisings" in aug_config.keys():
+            noise_transform = AddGaussianNoise(
+                std=aug_config["noisings"]["standard_deviation"]
+            )
+            transform_list += [noise_transform] * aug_config["noisings"]["num_noisings"]
+        if "translations" in aug_config.keys():
+            max_shift = aug_config["translations"]["max_shift"]
+            translate_transform = tvtf.RandomAffine(0, translate=(max_shift, max_shift))  # type: ignore
+            transform_list += [translate_transform] * aug_config["translations"][
+                "num_translations"
+            ]
+    transform_list = tuple(transform_list)
+
+    cnn_train_ds = Ellipse_Dataset(ds, transforms=transform_list)
+    cnn_valid_ds = Ellipse_Dataset(valid_ds)
+
+    batch_size = hyperparam_config["batch_size"]
+    cnn_train_dataloader = DataLoader(
+        cnn_train_ds, num_workers=8, batch_size=batch_size
+    )
+    cnn_valid_dataloader = DataLoader(
+        cnn_valid_ds, num_workers=8, batch_size=batch_size
+    )
+
+    cnn_model = CNN(cfg).to(device)
+    cnn_loss_fn = loss_function_translation[hyperparam_config["loss_function"]]()
+    cnn_optimizer = torch.optim.Adam(
+        cnn_model.parameters(), lr=hyperparam_config["learning_rate"]
+    )
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(cnn_optimizer, "min")
+
+    if hyperparam_config["early_stopping"]["enabled"]:
+        early_stopper = EarlyStopper(
+            patience=hyperparam_config["early_stopping"]["patience"],
+            min_delta=hyperparam_config["early_stopping"]["min_delta"],
+        )
+
+    summary(cnn_model, input_size=(batch_size, 1, 128, 128))
+
+    epochs = hyperparam_config["epochs"]
+    print("Training CNN \n")
+    t_time_start = perf_counter()
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n----------------------------")
+        start_time = perf_counter()
+        train(
+            cnn_train_dataloader,
+            cnn_model,
+            cnn_loss_fn,
+            cnn_optimizer,
+            accumulation_batches=hyperparam_config["accumulation_batches"],
+            device=device,
+        )
+        val_loss, val_accuracy = validate(
+            cnn_valid_dataloader, cnn_model, cnn_loss_fn, device=device
+        )
+        scheduler.step(val_loss)
+        if hyperparam_config["early_stopping"]["enabled"]:
+            if early_stopper.early_stop(val_loss, val_accuracy):
+                print(
+                    f"Early stopping with minimum validation loss of {early_stopper.min_validation_loss}"
+                    f", max accuracy of {early_stopper.max_accuracy}"
+                )
+                break
+            if early_stopper.counter == 0:
+                print(f"Validation loss improved, saving model")
+                torch.save(cnn_model.state_dict(), cfg.model_save_file)
+        else:
+            torch.save(cnn_model.state_dict(), cfg.model_save_file)
+
+        print(f"Epoch time: {perf_counter() - start_time:.2f} seconds \n")
+    t_time = perf_counter() - t_time_start
+    print(
+        f"Done! Total training time: {t_time // 60:.0f} minutes, {t_time % 60:.2f} seconds, average epoch time: {t_time/epochs:.2f} seconds"
+    )
+
+
+if __name__ == "__main__":
+    main()
